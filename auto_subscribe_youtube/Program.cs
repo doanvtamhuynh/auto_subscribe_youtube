cc4159a637ba5d4236c2e9827e7ea917000ae8e4
doanvtamhuynh <huynhdoan.2132@gmail.com>
2024-09-07 15:39:49 +0700

    add: models

--- auto_subscribe_youtube/Program.cs
+++ auto_subscribe_youtube/Program.cs
@@ -16,7 +16,7 @@ namespace auto_subscribe_youtube
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
